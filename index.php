67fe64892d48830f27ee7b4ceea17a9d843c40aa
KAnggara75 <KAnggara75@gmail.com>
2019-05-14 22:17:42 +0700

    Halaman Utama Pengalihan

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <title>
+        iVote
+    </title>
+    <link rel="stylesheet" href="http://localhost/iVote/public/css/bootstrap.css">
+    <link rel="stylesheet" href="http://localhost/iVote/public/css/w3.css">
+    <link rel="icon" type="image/png" href="http://localhost/iVote/public/img/icon.png">
+</head>
+
+<body>
+    <div class="container" align="center">
+    <div class="alert alert-danger" role="alert">
+    <h1>
+        Ini Bukan Halaman nya
+    </h1>
+    </div>
+    </div>
+
+    <p align="center">
+    Silahkan klik tombol lanjutkan di bawah ini adalah
+    </p>
+    <hr>
+    <div class="container" align="center">
+    <a href="http://localhost/iVote/public">
+        <button type="button" class="btn btn-primary btn-lg">Lanjutkan Ke iVote</button>
+    </a>
+    <br>
+    <br>
+    <a href="http://localhost/iVote/public/about">
+        <button type="button" class="btn btn-secondary btn-lg">Tentang Kami</button>
+    </a>
+    </div>
+    <br><br><br>
+    <div class="alert alert-danger" role="alert" align="center">
+        <marquee direction="left">
+        Out Of There!!!
+        </marquee>
+    </div>
+
+<div class="container-fluid">
+<div class="alert alert-dark" role="alert" align="center">
+        Nama Kelompok:<hr>
+        <marquee direction="left">
+        1. Adi Gunawan <br>
+        2. Agus Tami <br>
+        3. Fahzur Rahman <br>
+        4. Kelvin Anggara (KAnggara75) <br>
+        5. Riski Mangaraja <br>
+        6. Yudha Bagus Prasetya <br>
+        </marquee>
+    </div>
+</div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="http://localhost/iVote/public/js/bootstrap.js"></script>
+</body>
+
+</html>
